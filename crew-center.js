14de8cbe86839c954d0075406c119739ca30bd11
zcxr-gif <hrantony2230@gmail.com>
2025-10-05 14:00:22 -0400

    update: 3D modeling 5

--- crew-center.js
+++ crew-center.js
@@ -133,7 +133,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 
             const modelAsMercator = mapboxgl.MercatorCoordinate.fromLngLat(lngLat, 0);
             this.modelPosition = { x: modelAsMercator.x, y: modelAsMercator.y, z: modelAsMercator.z };
-            this.modelScale = modelAsMercator.meterInMercatorCoordinateUnits() * 20;
+            this.modelScale = modelAsMercator.meterInMercatorCoordinateUnits() * 2;
 
             this.loader.load(
                 modelPath,
